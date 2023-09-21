99d3f19e6e2c7b73a04a592e24f6d1330daf6d39
AllCanChange <85720410+AllCanChange@users.noreply.github.com>
2023-09-21 18:53:19 +0800

    Update snake.c

--- linux_learn/snake.c
+++ linux_learn/snake.c
@@ -11,22 +11,31 @@
  
  snack init_snake(snack * snack)
  {
-	 	 switch (snack->direct){
+	 struct snake snake;
+	 snake.direct=left;
+	 snake.snake_head_abscissa=0;
+	 snake.snake_head_ordinate=0;
+	
+		 
+		switch (snack->direct){
 		 case back:
-			snake_back();
+			snake_back(&snake);
 			break;
 		 case force:
-			snake_force();
+			snake_force(&snake);
 			break;
 		 case left:
-			snack_left();
+			snack_left(&snake);
 			break;
 		 case right:
-			snack_right();
+			snack_right(&snake);
 			break;
 		 default:
 			break;
-	 }
+		}
+	while(1){
+		snake_show(&snake);
+	}
  }
  
  
@@ -47,23 +56,31 @@
  
  void  snake_back(snack * snack)
  {
-	 if(snack->direct == left)
+	 if(snack->direct == force)
 		 return;
-	 snack->snake_head_abscissa++;
+	 snack->snake_head_ordinate++;
  }
  
  void snake_force(snack * snack)
  {
-	 if(snack->direct == left)
+	 if(snack->direct == back)
 		 return;
-	 snack->snake_head_abscissa++;
+	 snack->snake_head_ordinate--;
  }
  
  void snake_show(snack * snack)
  {
-	 int snake_map[20][20];
-
+	
+	int snake_map[abscissa_map][ordinate_map];
+	clc();
+	for(int i=0;i<snake_head_abscissa;i++)
+	{
+		for(int j=0;j<snake_head_ordinate;j++)
+		{
+			printf("*");
+		}
+	}
 	 
 	 printf("*");
-	 
+	sleep(500);
  } 
